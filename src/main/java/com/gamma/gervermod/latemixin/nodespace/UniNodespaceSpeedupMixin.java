2d8d4906584bb8153ddd4c7b9187d05cef14e242
BallOfEnergy01 <66693744+BallOfEnergy1@users.noreply.github.com>
2026-01-01 22:28:09 -0600

    Nodespace speedup since the fix didn't work.

--- src/main/java/com/gamma/gervermod/latemixin/nodespace/UniNodespaceSpeedupMixin.java
+++ src/main/java/com/gamma/gervermod/latemixin/nodespace/UniNodespaceSpeedupMixin.java
@@ -1,8 +1,11 @@
 package com.gamma.gervermod.latemixin.nodespace;
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.hbm.uninos.NodeNet;
 import com.hbm.uninos.UniNodespace;
 
-import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 
 @Mixin(UniNodespace.class)
 public abstract class UniNodespaceSpeedupMixin {
@@ -21,12 +25,32 @@ public abstract class UniNodespaceSpeedupMixin {
 
     @Inject(method = "<clinit>", at = @At("TAIL"), remap = false)
     private static void injected(CallbackInfo ci) {
-        activeNodeNets = new ObjectLinkedOpenHashSet<>();
+        activeNodeNets = new ObjectOpenHashSet<>();
     }
 
-    @Inject(method = "updateNodespace", at = @At("TAIL"), remap = false)
-    private static void injected2(CallbackInfo ci) {
-        // fix pulled from NTM main.
-        activeNodeNets.removeIf((net) -> net.links.isEmpty()); // reap empty networks
+    /**
+     * @author BallOfEnergy01
+     * @reason Performance fixes.
+     */
+    @Overwrite(remap = false)
+    public static void updateNetworks() {
+        Collection<NodeNet<?, ?, ?>> toRemove = new ObjectArrayList<>();
+
+        // more testing needed for parallelization.
+        Stream<NodeNet<?, ?, ?>> stream = activeNodeNets.stream()/* .parallel() */;
+
+        stream.forEach((net) -> {
+            if (net.links.isEmpty()) {
+                toRemove.add(net);
+                return;
+            }
+            net.resetTrackers();
+            net.update();
+        });
+
+        // the `toRemove` is very likely to be smaller than the `activeNodeNets`.
+        for (NodeNet<?, ?, ?> nodeNet : toRemove) {
+            activeNodeNets.remove(nodeNet);
+        }
     }
 }
